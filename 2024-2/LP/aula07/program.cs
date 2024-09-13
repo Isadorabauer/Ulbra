1f61762c03692e954fc630b5878f097d1374d3ff
Isadorabauer <isadora.bauer@rede.ulbra.br>
2024-09-13 18:59:34 -0300

    aula 7

---
+++ 2024-2/LP/aula07/program.cs
@@ -0,0 +1,9 @@
+Estoque baitaca = new Estoque();
+Console.WriteLine("Informe o nome do produto para adicionar ao estoque");
+baitaca.produto = Console.ReadLine();
+Console.WriteLine("Informe a quantidade de produto em estoque");
+baitaca.quantidade = Convert.ToInt32(Console.ReadLine());
+baitaca.AdicionarEstoque(quantidade);
+Console.WriteLine("Informe o valor do produto:");
+baitaca.preco = Convert.ToDouble(Console.ReadLine());
+baitaca.ExibeInformacoes();
